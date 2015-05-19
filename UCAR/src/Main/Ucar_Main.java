d4dc2496fc1db24a580f33a3ff60047544a42448
Administrator <Administrator@Sc-201504131508>
2015-05-19 10:32:32 +0900

    JGY.150519

--- UCAR/src/Main/Ucar_Main.java
+++ UCAR/src/Main/Ucar_Main.java
@@ -6,7 +6,7 @@ public class Ucar_Main {
 
 	public static void main(String[] args) {
 		
-		new Ucar_Init("UCAR 0.1.150517"); //메인 윈도우. (타이틀 버젼)
+		new Ucar_Init("UCAR 0.1.150519"); //메인 윈도우. (타이틀 버젼)
 		
 	}
 
