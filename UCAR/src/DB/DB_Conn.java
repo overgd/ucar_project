fc7b1ac379d1115b90c0abe578c6fc773ea83979
Administrator <Administrator@Sc-201504131508>
2015-05-21 09:41:24 +0900

    JGY.150521

--- UCAR/src/DB/DB_Conn.java
+++ UCAR/src/DB/DB_Conn.java
@@ -9,12 +9,14 @@ public class DB_Conn {
 	Statement stmt;
 	
 	String driver = "oracle.jdbc.driver.OracleDriver";
-	String url = "jdbc:oracle:thin:@192.168.43.103:1521:xe";
+//	String url = "jdbc:oracle:thin:@192.168.43.103:1521:xe";
+	String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
 	
 	public Connection getConnection() throws Exception {
 		
 		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url, "tm1", "1234");
+//		Connection con = DriverManager.getConnection(url, "tm1", "1234");
+		Connection con = DriverManager.getConnection(url, "hr", "hr");
 		stmt = con.createStatement();
 		return con;
 		
